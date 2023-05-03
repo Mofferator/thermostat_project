8df40801771702a72b35c037d9ce401cf8c45793
Joshua Moffat <moff@Joshuas-MacBook-Pro.local>
2023-05-03 09:49:43 -0400

    Added DDL for basic database to hold temperature data and device info

---
+++ mysql/initdb.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE Thermostat_Project;
+
+USE Thermostat_Project;
+
+CREATE TABLE Rooms (
+	room_id			INTEGER			NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	room_name		VARCHAR(128)	NOT NULL
+);
+
+CREATE TABLE Devices (
+	device_id 		INTEGER 		NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	device_name 	VARCHAR(64) 	NOT NULL,
+	location 		INTEGER,
+	CONSTRAINT device_room FOREIGN KEY (location) REFERENCES Rooms(room_id)
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT
+);
+
+CREATE TABLE Device_Data (
+	event_id		INTEGER 		NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	device_id 		INTEGER			NOT NULL,
+	date_info		DATETIME		NOT NULL,
+	temperature 	FLOAT			NOT NULL,
+	CONSTRAINT data_device FOREIGN KEY (device_id) REFERENCES Devices(device_id)
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT
+);
+
